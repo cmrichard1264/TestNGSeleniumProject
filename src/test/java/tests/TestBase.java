365ef715ad8331c39b5d9d53a1845c962f6b09d2
madinaturtbaeva <madina.turtbaeva@gmail.com>
2019-12-09 17:08:05 -0500

    update 12/09

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -30,8 +30,8 @@ public abstract class TestBase  {
     protected static ExtentTest extentTest;
 
     @BeforeTest
-    @Parameters("test")
-    public void beforeTest(@Optional String test){
+    @Parameters({"test", "env_url"})
+    public void beforeTest(@Optional String test, @Optional String env_url){
         //location of report
         //going to ne next to target folder, test-output folder
         String reportName = "report";
@@ -44,7 +44,11 @@ public abstract class TestBase  {
         extentReports.attachReporter(extentHtmlReporter);
         extentHtmlReporter.config().setReportName("Vytrack Test Results");
         //system information
-        extentReports.setSystemInfo("Environment", "QA1");
+        String env = ConfigurationReader.getProperty("url");
+        if(env_url != null){
+            env = env_url;
+        }
+        extentReports.setSystemInfo("Environment", env);
         extentReports.setSystemInfo("Browser", ConfigurationReader.getProperty("browser"));
         extentReports.setSystemInfo("OS", System.getProperty("os.name"));
     }
